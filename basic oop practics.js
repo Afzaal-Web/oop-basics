36c631b777a3be250a9da4856a0ba2bf198610bb
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-06-29 01:41:11 +0500

    Library system with basic oop

--- basic oop practics.js
+++ basic oop practics.js
@@ -1,7 +1,7 @@
-class UserProfile  {
+/* class UserProfile  {
 constructor(username, email){
     this.username = username;
-    this.email; email
+    this.email= email
 }
 displayInfo(){
     console.log(`Username: ${this.username} Email: ${this.email}`);
@@ -9,4 +9,90 @@ displayInfo(){
 }
 
 const user = new UserProfile('john', 'john@gmail.com');
-user.displayInfo();
+user.displayInfo(); */
+
+const libraryDisplay = document.getElementById('library-display');
+const bookTitle = document.getElementById('title');
+const author = document.getElementById('author');
+const getForm = document.getElementById('book-form');
+
+class Book {
+    constructor(title, author) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.isRead = true;
+    }
+    toggleReadStatus() {
+        this.isRead = !this.isRead;
+    }
+}
+
+class Library {
+    constructor() {
+        this.books = []
+    }
+    addBook(book) {
+        this.books.push(book);
+    }
+    removeBook(id) {
+        const index = this.books.findIndex((book) => book.id === id);
+        if (index !== -1) this.books.splice(index, 1);
+    }
+    listBooks() {
+        libraryDisplay.innerHTML = '';
+        if (this.books.length === 0) {
+            libraryDisplay.innerHTML = "<p>📚 No books in the library yet.</p>";
+            return;
+        }
+        this.books.forEach((book) => {
+            libraryDisplay.innerHTML += `
+            <div class="book-card">
+            <h3 class ="${book.isRead ? "read" : "unread"}">Book Title: ${book.title}</h3>
+            <h3 class ="${book.isRead ? "read" : "unread"}">Book Author: ${book.author}</h3>
+            <button class="toggle-status" data-id="${book.id}"> Mark as ${book.isRead ? "Unread " : "Read"}</button>
+            <button class="remove-book" data-id="${book.id}">Remove</button>
+            </div>
+`;
+        });
+        bookTitle.value = '';
+        author.value = '';
+        attachEventHandlers();
+    }
+}
+
+const library = new Library();
+library.listBooks();
+
+getForm.addEventListener('submit', () => {
+    event.preventDefault();
+    if (!bookTitle.value.trim() && !author.value.trim()) {
+        return;
+    } else {
+        const addBooks = new Book(bookTitle.value.trim(), author.value.trim());
+        library.addBook(addBooks);
+        library.listBooks();
+    }
+
+});
+
+function attachEventHandlers() {
+    const toggleStatus = document.querySelectorAll('.toggle-status');
+    toggleStatus.forEach((button) => {
+        button.addEventListener('click', () => {
+            const getStatus = library.books.find(b => b.id === button.dataset.id);
+            getStatus.toggleReadStatus();
+            library.listBooks();
+        });
+    });
+    const removeBook = document.querySelectorAll('.remove-book');
+    removeBook.forEach((button) => {
+        button.addEventListener('click', () => {
+            const id = button.dataset.id;
+            if (confirm("Are you sure you want to delete this book?")) {
+                library.removeBook(id);
+                library.listBooks();
+            }
+        });
+    });
+}
